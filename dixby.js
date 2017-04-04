378c809b7d819f2aad092b9ad270baf2e35d6142
Mary E. Knight <MarySunshine@Marys-MacBook-Pro.local>
2017-04-04 17:05:12 -0400

    spotify api works

--- dixby.js
+++ dixby.js
@@ -1,16 +1,33 @@
+//When creating this application remember:
+//Use conditionals - if the user does __ then it will do ____
+
+
 console.log("hey!")
 
 //basic skeleton for how to structure the spotify API package using Node 
 
 var spotify = require('spotify');
  
-spotify.search({ type: 'track', query: 'dirty diana' }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
+//brackets let js identify the index of an array
+
+// the if conditional below states that if the second index in the command line is = to the string which is "spotify-this-song" then do everything listed in the function
+ if (process.argv[2] === "spotify-this-song") {
+
+//only use == and === in if/then statements not when declaring/designating variable
+
+var songName = process.argv[3]
+	console.log(songName)
+
+	spotify.search({ type: 'track', query: songName }, function(err, data) {
+    	if ( err ) {
+        	console.log('Error occurred: ' + err);
         return;
-    }
+    	}
  
     // Do something with 'data' 
     console.log(data)
 
-});
+	});
+}
+	
+
